e4c3603231a2075d73229e4831b9c9b478c0d2e3
PerizatSabitbek <psabitbek@magnumtech.kz>
2021-11-05 18:59:37 +0600

    install sass

--- src/app/index.js
+++ src/app/index.js
@@ -3,5 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./components/app";
 
 
-console.log("it works")
+
 ReactDOM.render(<App /> , document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,35 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const currentTask = "build";
 
-module.exports = {
-  entry: "./src/app/index.js",
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "index_bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+const config = {
+    entry: "./src/app/index.js",
+    output: {
+      path: path.join(__dirname, "/dist"),
+      filename: "index_bundle.js",
+    },
+    mode: "development",
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
         },
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
+        {
+          test: /\.scss$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
-};
+  };
+
+module.exports = config;
