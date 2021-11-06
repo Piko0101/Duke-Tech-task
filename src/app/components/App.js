9d0c0b18297ab426d5f5f412fa5c8432aa93d985
Perizat Sabitbek <sonepiko@gmail.com>
2021-11-06 10:49:12 +0600

    create components

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import '../styles/App.scss'
+import Button from './shared/Button'
 
 function App() {
     return (
         <div>
-            <h1>Hello world</h1>
+            <Button />
         </div>
     )
 }
