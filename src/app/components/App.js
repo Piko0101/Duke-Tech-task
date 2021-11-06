54e9372e2c3790502d908fc52e6aa500c1eecb76
Perizat Sabitbek <sonepiko@gmail.com>
2021-11-06 12:40:05 +0600

    create searchInput component

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../styles/App.scss'
 import Button from './shared/Button'
+import SearchInput from './shared/SearchInput'
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
     }
     return (
         <div>
-            
+            <SearchInput />
         </div>
     )
 }
--- src/app/components/shared/SearchInput.js
+++ src/app/components/shared/SearchInput.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Button from './Button'
+import '../../styles/SearchInput.scss'
 
 function SearchInput() {
     return (
         <div className="search-input">
-            <input></input>
-            <Button />
+            <input placeholder="Поиск по названию картины" />
+            <Button className="button_main" children="Найти"/>
         </div>
     )
 }
